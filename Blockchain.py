a7d9acc542b0d8a3eda833349b593cda4d74133b
Udit Kaul <uditkaul@outlook.com>
2019-05-28 16:45:54 +0100

    Added function for the follow : 1) To generate Hash function 2) To check if the blockchain is valid

--- Blockchain.py
+++ Blockchain.py
@@ -21,7 +21,7 @@ class blockchain:
         new_proof =1
         check_proof=False
         while check_proof is False:
-            # Used a non-symmetric arithmetic so that the result of the arithmetic operation does not repeat
+            # Used a non-symmetric arithmetic so that the result of the arithmetic operation is not repititive
             # The idea is to make the operation tough to compute but easy to check 
             #str and encode functions are used to make the value compatible to what the hashlib.sha256() function is expecting
             # The hexdigest() function is for making the value in hexadecimal form
@@ -34,3 +34,26 @@ class blockchain:
             else:
                 new_proof+=1
         return new_proof
+    #Hash Function to get convert the existing blocks into their respective hashes
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    #Check if each block in the blockchain has the correct proof of work
+    #Check if Previous Hash of each block is equal to the hash of previous block
+    #Thereby making sure we have a valid blockchain
+    def is_chain_valid(self, chain):
+        previous_block=chain[0]
+        block_index=1
+        while block_index<= len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_function = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_function[:4]!='0000':
+                return False
+            previous_block = block
+            block_index+=1
+            return True
