2ceb702f1be790799374aaea4c4961306daaf959
Udit Kaul <uditkaul@outlook.com>
2019-05-28 14:53:34 +0100

    Inital Commit which added 1) __init__ function 2) Create Block Function 3) Get Previous Block Function

---
+++ Blockchain.py
@@ -0,0 +1,17 @@
+import datetime
+import hashlib
+import json
+from flask import flask, jsonify
+
+class blockchain:
+    def __init__(self):
+        self.chain=[]
+        self.create_block(proof=1, previous_hash='0')
+    
+    def create_block(self, proof, previous_hash):
+        block={'index':len(self.chain)+1,'timestamp':str(datetime.datetime.now()),'proof':proof,'previous_hash':previous_hash }
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]  
