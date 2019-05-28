a34fc3d5f49127e942307c660b1751a6816de284
Udit Kaul <uditkaul@outlook.com>
2019-05-28 15:36:01 +0100

    Added method to check proof of work

--- Blockchain.py
+++ Blockchain.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 from flask import flask, jsonify
 
+
 class blockchain:
     def __init__(self):
         self.chain=[]
@@ -15,4 +16,21 @@ class blockchain:
     def get_previous_block(self):
         return self.chain[-1]
    
-   
+   #This function is to check if the proof of work mined is valid 
+    def proof_of_work(self, previous_proof):
+        new_proof =1
+        check_proof=False
+        while check_proof is False:
+            # Used a non-symmetric arithmetic so that the result of the arithmetic operation does not repeat
+            # The idea is to make the operation tough to compute but easy to check 
+            #str and encode functions are used to make the value compatible to what the hashlib.sha256() function is expecting
+            # The hexdigest() function is for making the value in hexadecimal form
+            hash_function = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+
+            #The check is for 4 leading zeros in this case as it any hash start with 4 leading zeros is considers a golden nuance
+            #Since this is not a realtime blockchain the value is kept easier to compute for any system
+            if hash_function[:4]=='0000':
+                check_proof=True
+            else:
+                new_proof+=1
+        return new_proof
