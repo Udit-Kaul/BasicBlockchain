f002986f2b117629951c1e3414af7e51342e0be6
Udit Kaul <uditkaul@outlook.com>
2019-05-29 17:01:57 +0100

    Added route to check if the blockchain is valid and code to run the app

--- Blockchain.py
+++ Blockchain.py
@@ -1,10 +1,10 @@
-#Importing required libraries
+# Importing required libraries
 import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
 
-#Building a Blockchain
+# Building a Blockchain
 class Blockchain:
     def __init__(self):
         self.chain=[]
@@ -17,14 +17,14 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
    
-   #This function is to check if the proof of work mined is valid 
+   # This function is to check if the proof of work mined is valid 
     def proof_of_work(self, previous_proof):
         new_proof =1
         check_proof=False
         while check_proof is False:
             # Used a non-symmetric arithmetic so that the result of the arithmetic operation is not repititive
             # The idea is to make the operation tough to compute but easy to check 
-            #str and encode functions are used to make the value compatible to what the hashlib.sha256() function is expecting
+            # str and encode functions are used to make the value compatible to what the hashlib.sha256() function is expecting
             # The hexdigest() function is for making the value in hexadecimal form
             hash_function = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
 
@@ -35,18 +35,18 @@ class Blockchain:
             else:
                 new_proof+=1
         return new_proof
-    #Hash Function to get convert the existing blocks into their respective hashes
+    # Hash Function to get convert the existing blocks into their respective hashes
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
-    #Check if each block in the blockchain has the correct proof of work
-    #Check if Previous Hash of each block is equal to the hash of previous block
-    #Thereby making sure we have a valid blockchain
+    # Check if each block in the blockchain has the correct proof of work
+    # Check if Previous Hash of each block is equal to the hash of previous block
+    # Thereby making sure we have a valid blockchain
     def is_chain_valid(self, chain):
         previous_block=chain[0]
         block_index=1
-        while block_index<= len(chain):
+        while block_index< len(chain):
             block = chain[block_index]
             if block['previous_hash'] != self.hash(previous_block):
                 return False
@@ -58,6 +58,7 @@ class Blockchain:
             previous_block = block
             block_index+=1
             return True
+    
 # Mining Blockchain
 
 # Creating a simple webapp using Flask
@@ -77,14 +78,31 @@ def mine_block():
     response={'message':'Congratulations !! You Have Successfully Mined The Block',
     'index':block['index'],
     'timestamp':block['timestamp'],
-    proof:block['proof'],
-    previous_hash:block['previous_hash']}
+    'proof':block['proof'],
+    'previous_hash':block['previous_hash']}
     
     return jsonify(response),200
 
 # Getting the Entire Blockchain
+
 @app.route('/get_chain',methods=['GET'])
 def get_chain():
     response={'chain':blockchain.chain,
                 'length':len(blockchain.chain)}
     return jsonify(response),200
+
+# Checking if the blockchain is valid
+
+@app.route('/is_valid',methods=['GET'])
+def is_valid():
+    isValid = blockchain.is_chain_valid(blockchain.chain)
+    
+    if isValid:
+        response={'message':'All Good. The Blockchain is valid !!'}
+    else:
+        response={'message':'Houston, we have a problem. The Blockchain is not valid !!'}
+    return jsonify(response),200
+
+# Running the app
+
+app.run(host='0.0.0.0', port=5000)
